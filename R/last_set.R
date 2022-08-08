14d1203ba18d4bb65fd75da9e35cd9ca7378c72d
Alessandro Balconi <alessandro.balconi@outlook.it>
2022-08-08 13:22:40 +0000

    added last_set() function

--- R/last_set.R
+++ R/last_set.R
@@ -6,5 +6,23 @@
 #' @examples
 #' last_set()
 last_set <- function() {
-  'idk'
+
+  # perform GET request
+  req <- httr::GET("https://dd.b.pvp.net/latest/core/en_us/data/globals-en_us.json")
+
+  # extract content from the GET
+  content <- httr::content(req, encoding = "UTF-8")
+
+  # extract sets name from JSON content
+  sets <- jsonlite::fromJSON(content)[["sets"]]$nameRef
+
+  # extract number from sets and convert to numeric
+  sets <- as.numeric(stringi::stri_extract(sets, regex = '[0-9]+'))
+
+  # get maximum number (it's the latest set released)
+  last_set <- max(sets, na.rm = TRUE)
+
+  # return last_set
+  return(last_set)
+
 }
