3ba0f3b125b68dad59f1a8514d92e2eeedf1610d
Alessandro Balconi <alessandro.balconi@outlook.it>
2022-08-16 09:22:28 +0000

    added "bigint" parameter

--- R/create_db_con.R
+++ R/create_db_con.R
@@ -1,11 +1,13 @@
 #' Connect to MySQL database
 #'
+#' @param bigint Class to use for numeric values (one of integer", "numeric", "character", "integer64")
+#'
 #' @return A connection object
 #' @export
 #'
 #' @examples
 #' create_db_con()
-create_db_con <- function() {
+create_db_con <- function(bigint = "integer64") {
 
   # load mysql db credentials
   db_creds <- config::get("mysql", file = "/home/balco/my_rconfig.yml")
@@ -16,7 +18,8 @@ create_db_con <- function() {
     db_host  = "127.0.0.1",
     user     = db_creds$uid,
     password = db_creds$pwd,
-    dbname   = db_creds$dbs
+    dbname   = db_creds$dbs,
+    bigint = bigint
   )
 
   # return connection
