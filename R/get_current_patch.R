0005dbbef1adbe55c6bea2122a1ca552829f9002
Alessandro Balconi <alessandro.balconi@outlook.it>
2022-08-16 16:57:24 +0000

    added get_patch_release_date() function

--- R/get_current_patch.R
+++ R/get_current_patch.R
@@ -1,6 +1,6 @@
 #' Get info on the most recent patch released
 #'
-#' @param what One of "patch", "patch_regex", "release_date" (default: "patch")
+#' @param what One of "patch", "patch_regex" (default: "patch")
 #' @param since_last_change If TRUE, returns all patches since the last one with changes; otherwise returns only the very last one (default: TRUE)
 #' @param collapse If since_last_change = TRUE, should patches be collapsed into a single character string? (default: TRUE)
 #' @param sep separator to use for collapsing when collapse = TRUE (default: "|")
@@ -61,5 +61,47 @@ get_current_patch <- function(
 
   # return result
   return(res)
+
+}
+
+#' Get info on the release date of a patch
+#'
+#' @param patch The patch to check, in the format "live_X_XX" (if NULL, uses the latest patch since changes)
+#' @param on_live If TRUE, adds 1 day to return the date when the patch actually went live (default: TRUE)
+#'
+#' @return A date
+#' @export
+get_patch_release_date <- function(
+  patch = NULL,
+  on_live = TRUE
+) {
+
+  # if patch = NULL, fetch most recent patch
+  if(is.null(patch)){
+    patch <- lorr::get_current_patch(what = 'patch_regex')
+  }
+
+  # get first occurence of patch in the data
+  release_date <- lorr::get_db_query(
+    query = "
+    SELECT
+      MIN(game_start_time_utc)
+    FROM
+      ranked_match_metadata_30d
+    WHERE
+      game_version REGEXP '{patch}'
+    ",
+    patch = patch
+  )
+
+  release_date <- release_date[[1]]
+
+  # if needed, add 1 day to get the actual patch release date
+  if(on_live){
+    release_date <- release_date + 86400
+  }
+
+  # return result
+  return(release_date)
 
 }
