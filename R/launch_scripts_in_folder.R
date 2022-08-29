6c34f20d2a54adf52f94eaec8acb040cc8a9f8b1
Alessandro Balconi <alessandro.balconi@outlook.it>
2022-08-29 15:43:09 +0000

    added "launch_scripts_in_folder()" function

---
+++ R/launch_scripts_in_folder.R
@@ -0,0 +1,42 @@
+#' Run all scripts inside a folder, sending a discord message if they fail
+#'
+#' @param folder Path to the folder with the scripts to run
+#' @param log_time boolean, should we keep track of the time needed to run the script? (default: TRUE)
+#' @param log_message Message to send alongside the elapsed time in logs
+#' @param send_error_to_discord boolean, should a discord message be sent if the scripts fail? (default: TRUE)
+#' @param discord_username The username that will be shown as sender on Discord
+#' @param discord_message The content of the message sent on Discord
+#'
+#' @return Nothing, just runs the scripts
+#' @export
+launch_scripts_in_folder <- function(
+  folder,
+  log_time = TRUE,
+  log_message = 'End: ',
+  send_error_to_discord = TRUE,
+  discord_username,
+  discord_message = "Error. "
+){
+
+  # list all scripts in the folder
+  scripts <- list.files(folder, full.names = TRUE)
+
+  if(log_time) { tictoc::tic() }
+
+  # safely run the scripts, returning an error message if something goes wrong
+  tryCatch(
+    expr = purrr::walk(.x = scripts, .f = source),
+    error = function(e) {
+      if(send_error_to_discord){
+        lorr::send_discord_message(
+          username = discord_username,
+          message = sprintf("%s(%s)", discord_message, e$message)
+        )
+      }
+      print(e)
+    }
+  )
+
+  if(log_time) { cat(log_message); tictoc::toc() }
+
+}
