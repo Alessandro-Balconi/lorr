490a977870079475ea2c11db98a8612eb7353046
Alessandro Balconi <alessandro.balconi@outlook.it>
2022-10-14 18:51:19 +0000

    added "use_latest" parameter to function to allow using versioned jsons

--- R/get_cards_data.R
+++ R/get_cards_data.R
@@ -27,19 +27,28 @@ get_sets <- function(trim = TRUE) {
 #'
 #' @param set The number of the set
 #' @param select A character vector with the fields to keep (return all fields if NULL)
+#' @param use_latest Should the "latest" JSON be used? or the current version one? (default: TRUE)
 #'
 #' @return A dataframe
 #' @export
 get_set_cards_data <- function(
   set,
-  select = NULL
+  select = NULL,
+  use_latest = TRUE
   ) {
+
+  patch <- ifelse(
+    use_latest,
+    "latest",
+    paste0(gsub('\\.', '_', lorr::get_current_patch(since_last_change = FALSE)), "_0")
+  )
 
   # perform GET request
   req <- httr::GET(
     sprintf(
-      "https://dd.b.pvp.net/latest/set%1$s/en_us/data/set%1$s-en_us.json",
-      set
+      "https://dd.b.pvp.net/%2$s/set%1$s/en_us/data/set%1$s-en_us.json",
+      set,
+      patch
     )
   )
 
@@ -48,6 +57,14 @@ get_set_cards_data <- function(
 
   # extract data from JSON content
   data <- jsonlite::fromJSON(content)
+
+  # # FOR SET6CDE, RIOT SPLIT CARDS INTO 2 SETS >.>
+  # if(set == "6cde"){
+  #   req2 <- httr::GET("https://dd.b.pvp.net/3_16_0/set6cde/en_us/data/set6cde-en_us.json")
+  #   content2 <- httr::content(req2, encoding = "UTF-8")
+  #   data2 <- jsonlite::fromJSON(content2)
+  #   data <- rbind(data, data2)
+  # }
 
   # if needed, keep only some columns
   if(!is.null(select)){ data <- data[select] }
@@ -61,10 +78,11 @@ get_set_cards_data <- function(
 #' Get all data about cards
 #'
 #' @param select A character vector with the fields to keep (return all fields if NULL)
+#' @param use_latest Should the "latest" JSON be used? or the current version one? (default: TRUE)
 #'
 #' @return A dataframe
 #' @export
-get_cards_data <- function(select = NULL) {
+get_cards_data <- function(select = NULL, use_latest = TRUE) {
 
   # Get list of all sets released on LoR
   # "Event" is a special set that has no cards
@@ -73,7 +91,7 @@ get_cards_data <- function(select = NULL) {
   # pull data for all sets and bind them
   data <- purrr::map_dfr(
     .x = purrr::set_names(sets, sets),
-    .f = ~lorr::get_set_cards_data(., select = select),
+    .f = ~lorr::get_set_cards_data(., select = select, use_latest = use_latest),
     .id = "set"
   )
 
